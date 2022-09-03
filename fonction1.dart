6987db504fa11dadc481d50de088204fbc4f1360
ClichyMercury <gaelelscientifico@outlook.com>
2022-09-03 16:55:00 +0000

    repo

---
+++ fonction1.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main(List<String> args) {}
+
+void function(
+  int a,
+  int b,
+  int c,
+) {
+  int? a;
+  int? b;
+  int? c;
+
+  if (sqrt(a!) + sqrt(b!) == sqrt(c!)) {
+    print('rectangle');
+  } else {
+    print('pas rectangle');
+  }
+}
---
+++ fonction3.dart
@@ -0,0 +1,19 @@
+import 'fonction1.dart';
+
+void main() {}
+
+void function(
+  tab,
+  int i,
+) {
+  var tab = [4, 34, 56, 786, -7, -4, -9];
+  var tabplus = [];
+  var tabmoins = [];
+  for (var i in tab) {
+    if (tab[i] < 0) {
+      tabmoins = tab[i] as List;
+    } else {
+      tabmoins = tab[i] as List;
+    }
+  }
+}
